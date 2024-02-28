bb27670b70e30adb3adfa684110cce5b71731acb
Kudrotey <kudrotey.khuda@madetech.com>
2024-02-28 07:42:07 +0000

    Makes first test pass.

--- excercise/tests/test_bottles.py
+++ excercise/tests/test_bottles.py
@@ -1,20 +1,13 @@
-def test():
-    pass
+from ..bottles import Bottles
 
-# gem 'minitest', '~> 5.4'
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require_relative '../lib/bottles'
+def test_the_first_verse():
+    expected = "99 bottles of beer on the wall, " + \
+        "99 bottles of beer.\n" + \
+        "Take one down and pass it around, " + \
+        "98 bottles of beer on the wall.\n"
+    
+    assert expected == Bottles().verse(99)
 
-# class BottlesTest < Minitest::Test
-#   def test_the_first_verse
-#     expected =
-#       "99 bottles of beer on the wall, " +
-#       "99 bottles of beer.\n" +
-#       "Take one down and pass it around, " +
-#       "98 bottles of beer on the wall.\n"
-#     assert_equal expected, Bottles.new.verse(99)
-#   end
 
 #   def test_another_verse
 #     expected =
