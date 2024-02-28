46b4f5cc87dc3775c7a8c924ae17b47bedc16b12
Kudrotey <kudrotey.khuda@madetech.com>
2024-02-28 07:50:11 +0000

    Make fifth test pass.

--- excercise/tests/test_bottles.py
+++ excercise/tests/test_bottles.py
@@ -34,14 +34,13 @@ def test_verse_1():
 
     assert expected == Bottles().verse(1)
 
-#   def test_verse_0
-#     expected =
-#       "No more bottles of beer on the wall, " +
-#       "no more bottles of beer.\n" +
-#       "Go to the store and buy some more, " +
-#       "99 bottles of beer on the wall.\n"
-#     assert_equal expected, Bottles.new.verse(0)
-#   end
+def test_verse_0():
+    expected = "No more bottles of beer on the wall, " + \
+        "no more bottles of beer.\n" + \
+        "Go to the store and buy some more, " + \
+        "99 bottles of beer on the wall.\n"
+
+    assert expected, Bottles().verse(0)
 
 #   def test_a_couple_verses
 #     expected =
