39628bb004cbef1ac772d9fadf69c5f4cb28b76e
Kudrotey <kudrotey.khuda@madetech.com>
2024-02-29 13:02:33 +0000

    Test verse 2.

--- chapter2/bottles.py
+++ chapter2/bottles.py
@@ -1,6 +1,12 @@
 class Bottles():
-    def verse(self, number_of_bottles):
-        return f"{number_of_bottles} bottles of milk on the wall, " + \
-               f"{number_of_bottles} bottles of milk.\n" + \
-               f"Take one down and pass it around, " + \
-               f"{number_of_bottles - 1} bottles of milk on the wall.\n"
+    def verse(self, number):
+        if number == 2:
+            return f"2 bottles of milk on the wall, " + \
+                   f"2 bottles of milk.\n" + \
+                   f"Take one down and pass it around, " + \
+                   f"1 bottle of milk on the wall.\n"
+        else:
+            return f"{number} bottles of milk on the wall, " + \
+                   f"{number} bottles of milk.\n" + \
+                   f"Take one down and pass it around, " + \
+                   f"{number - 1} bottles of milk on the wall.\n"
--- chapter2/tests/test_bottles.py
+++ chapter2/tests/test_bottles.py
@@ -1,5 +1,6 @@
 from ..bottles import Bottles
 
+
 def test_the_first_verse():
     expected = "99 bottles of milk on the wall, " + \
                "99 bottles of milk.\n" + \
@@ -8,10 +9,20 @@ def test_the_first_verse():
     actual = Bottles().verse(99)        
     assert expected == actual
     
+    
 def test_another_verse():
     expected = "3 bottles of milk on the wall, " + \
                "3 bottles of milk.\n" + \
                "Take one down and pass it around, " + \
                "2 bottles of milk on the wall.\n"
     actual = Bottles().verse(3)        
+    assert expected == actual
+    
+    
+def test_verse_2():
+    expected = "2 bottles of milk on the wall, " + \
+               "2 bottles of milk.\n" + \
+               "Take one down and pass it around, " + \
+               "1 bottle of milk on the wall.\n"
+    actual = Bottles().verse(2)        
     assert expected == actual
