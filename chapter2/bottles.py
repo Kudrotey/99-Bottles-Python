a8eca957d9a65733aefba7e5689d55fb0a1a10b6
Kudrotey <kudrotey.khuda@madetech.com>
2024-02-29 12:42:24 +0000

    Make verse method more dyanmic.

--- chapter2/bottles.py
+++ chapter2/bottles.py
@@ -1,6 +1,6 @@
 class Bottles():
-    def verse(self, _):
-        return "99 bottles of milk on the wall, " + \
-               "99 bottles of milk.\n" + \
-               "Take one down and pass it around, " + \
-               "98 bottles of milk on the wall.\n"
+    def verse(self, number_of_bottles):
+        return f"{number_of_bottles} bottles of milk on the wall, " + \
+               f"{number_of_bottles} bottles of milk.\n" + \
+               f"Take one down and pass it around, " + \
+               f"{number_of_bottles - 1} bottles of milk on the wall.\n"
--- chapter2/tests/test_bottles.py
+++ chapter2/tests/test_bottles.py
@@ -7,3 +7,11 @@ def test_the_first_verse():
                "98 bottles of milk on the wall.\n"
     actual = Bottles().verse(99)        
     assert expected == actual
+    
+def test_another_verse():
+    expected = "3 bottles of milk on the wall, " + \
+               "3 bottles of milk.\n" + \
+               "Take one down and pass it around, " + \
+               "2 bottles of milk on the wall.\n"
+    actual = Bottles().verse(3)        
+    assert expected == actual
