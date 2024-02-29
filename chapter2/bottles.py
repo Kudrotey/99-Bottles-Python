8b179ecf1390bede7267dfc8dd56bb0c8cfe72f5
Kudrotey <kudrotey.khuda@madetech.com>
2024-02-29 12:29:15 +0000

    Tests first verse.

---
+++ chapter2/bottles.py
@@ -0,0 +1,6 @@
+class Bottles():
+    def verse(self, _):
+        return "99 bottles of milk on the wall, " + \
+               "99 bottles of milk.\n" + \
+               "Take one down and pass it around, " + \
+               "98 bottles of milk on the wall.\n"
--- chapter2/tests/test_bottles.py
+++ chapter2/tests/test_bottles.py
@@ -1,2 +1,9 @@
-def test():
-    pass
+from ..bottles import Bottles
+
+def test_the_first_verse():
+    expected = "99 bottles of milk on the wall, " + \
+               "99 bottles of milk.\n" + \
+               "Take one down and pass it around, " + \
+               "98 bottles of milk on the wall.\n"
+    actual = Bottles().verse(99)        
+    assert expected == actual
