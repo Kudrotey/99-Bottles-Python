82fa6f6e4a9e322ee158f2291e7f05086e356f21
Kudrotey <kudrotey.khuda@madetech.com>
2024-03-01 12:19:18 +0000

    Tests all variants of verses.

--- chapter2/bottles.py
+++ chapter2/bottles.py
@@ -1,12 +1,23 @@
 class Bottles():
     def verse(self, number):
-        if number == 2:
-            return f"2 bottles of milk on the wall, " + \
-                   f"2 bottles of milk.\n" + \
-                   f"Take one down and pass it around, " + \
-                   f"1 bottle of milk on the wall.\n"
-        else:
-            return f"{number} bottles of milk on the wall, " + \
-                   f"{number} bottles of milk.\n" + \
-                   f"Take one down and pass it around, " + \
-                   f"{number - 1} bottles of milk on the wall.\n"
+        match number:
+            case 0:
+                return "No more bottles of milk on the wall, " + \
+                       "no more bottles of milk.\n" + \
+                       "Go to the store and buy some more, " + \
+                       "99 bottles of milk on the wall.\n"
+            case 1:
+                return "1 bottle of milk on the wall, " + \
+                       "1 bottle of milk.\n" + \
+                       "Take one down and pass it around, " + \
+                       "no more bottle of milk on the wall.\n"
+            case 2:
+                return "2 bottles of milk on the wall, " + \
+                       "2 bottles of milk.\n" + \
+                       "Take one down and pass it around, " + \
+                       "1 bottle of milk on the wall.\n"
+            case _:
+                return f"{number} bottles of milk on the wall, " + \
+                       f"{number} bottles of milk.\n" + \
+                       f"Take one down and pass it around, " + \
+                       f"{number - 1} bottles of milk on the wall.\n"
--- chapter2/tests/test_bottles.py
+++ chapter2/tests/test_bottles.py
@@ -26,3 +26,21 @@ def test_verse_2():
                "1 bottle of milk on the wall.\n"
     
     assert expected == Bottles().verse(2)
+    
+    
+def test_verse_1():
+    expected = "1 bottle of milk on the wall, " + \
+               "1 bottle of milk.\n" + \
+               "Take one down and pass it around, " + \
+               "no more bottle of milk on the wall.\n"
+    
+    assert expected == Bottles().verse(1)
+    
+    
+def test_verse_0():
+    expected = "No more bottles of milk on the wall, " + \
+               "no more bottles of milk.\n" + \
+               "Go to the store and buy some more, " + \
+               "99 bottles of milk on the wall.\n"
+    
+    assert expected == Bottles().verse(0)
