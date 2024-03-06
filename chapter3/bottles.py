d4c316562325c751e8fcd91b50c758021f24356a
Kudrotey <kudrotey.khuda@madetech.com>
2024-03-06 07:50:09 +0000

    Removes case 2.

--- chapter3/bottles.py
+++ chapter3/bottles.py
@@ -21,11 +21,6 @@ class Bottles():
                        "1 bottle of milk.\n" + \
                        "Take one down and pass it around, " + \
                        "no more bottles of milk on the wall.\n"
-            case 2:
-                return f"{number} bottles of milk on the wall, " + \
-                       f"{number} bottles of milk.\n" + \
-                       f"Take one down and pass it around, " + \
-                       f"{number - 1} {self.container(number - 1)} of milk on the wall.\n"
             case _:
                 return f"{number} bottles of milk on the wall, " + \
                        f"{number} bottles of milk.\n" + \
