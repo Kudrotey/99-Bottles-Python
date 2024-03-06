6beb8a90b40571156033a0e258567b99b2233801
Kudrotey <kudrotey.khuda@madetech.com>
2024-03-06 07:15:38 +0000

    Refactor: Updates case 2 to use number variable.

--- chapter3/bottles.py
+++ chapter3/bottles.py
@@ -22,10 +22,10 @@ class Bottles():
                        "Take one down and pass it around, " + \
                        "no more bottles of milk on the wall.\n"
             case 2:
-                return "2 bottles of milk on the wall, " + \
-                       "2 bottles of milk.\n" + \
-                       "Take one down and pass it around, " + \
-                       "1 bottle of milk on the wall.\n"
+                return f"{number} bottles of milk on the wall, " + \
+                       f"{number} bottles of milk.\n" + \
+                       f"Take one down and pass it around, " + \
+                       f"{number - 1} bottle of milk on the wall.\n"
             case _:
                 return f"{number} bottles of milk on the wall, " + \
                        f"{number} bottles of milk.\n" + \
