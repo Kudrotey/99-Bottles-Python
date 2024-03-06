b10855a2da3940805d0b8d50af3c727dff5c117c
Kudrotey <kudrotey.khuda@madetech.com>
2024-03-06 07:47:57 +0000

    Updates branch 2 to use condition method.

--- chapter3/bottles.py
+++ chapter3/bottles.py
@@ -25,7 +25,7 @@ class Bottles():
                 return f"{number} bottles of milk on the wall, " + \
                        f"{number} bottles of milk.\n" + \
                        f"Take one down and pass it around, " + \
-                       f"{number - 1} bottle of milk on the wall.\n"
+                       f"{number - 1} {self.container(number - 1)} of milk on the wall.\n"
             case _:
                 return f"{number} bottles of milk on the wall, " + \
                        f"{number} bottles of milk.\n" + \
