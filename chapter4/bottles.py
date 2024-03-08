ca5f7763f2006e79b53515fec6e5daf20e1dfc4a
Kudrotey <kudrotey.khuda@madetech.com>
2024-03-08 07:17:16 +0000

    Refactor: make case_1 first line identical to else branch.

--- chapter4/bottles.py
+++ chapter4/bottles.py
@@ -14,17 +14,12 @@ class Bottles():
     def verse(self, number:int) -> str:
         match number:
             case 0:
-                return "No more bottles of milk on the wall, " + \
-                       "no more bottles of milk.\n" + \
-                       "Go to the store and buy some more, " + \
-                       "99 bottles of milk on the wall.\n"
-            case 1:
-                return f"{number} {self.container(number)} of milk on the wall, " + \
-                       f"{number} {self.container(number)} of milk.\n" + \
-                       f"Take {self.pronoun(number)} down and pass it around, " + \
-                       f"{self.quantity(number - 1)} {self.container(number - 1)} of milk on the wall.\n"
+                return f"{self.quantity(number).capitalize()} {self.container(number)} of milk on the wall, " + \
+                       f"no more bottles of milk.\n" + \
+                       f"Go to the store and buy some more, " + \
+                       f"99 bottles of milk on the wall.\n"
             case _:
-                return f"{number} {self.container(number)} of milk on the wall, " + \
+                return f"{self.quantity(number).capitalize()} {self.container(number)} of milk on the wall, " + \
                        f"{number} {self.container(number)} of milk.\n" + \
                        f"Take {self.pronoun(number)} down and pass it around, " + \
                        f"{self.quantity(number - 1)} {self.container(number - 1)} of milk on the wall.\n"
@@ -39,7 +34,7 @@ class Bottles():
             return 'it'
         return 'one'
     
-    def quantity(self, number:int) -> Union[str, int]:
+    def quantity(self, number:int) -> str:
         if number == 0:
             return 'no more'
-        return number
+        return str(number)
